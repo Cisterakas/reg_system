f0a98f77eaf6092eb2cd2ca7cbb0ca9400cd980a
Cisterakas <jcister_220000001482@uic.edu.ph>
2023-11-03 03:59:56 +0800

    ship

--- src/components/ApplicationNavigationBar.vue
+++ src/components/ApplicationNavigationBar.vue
@@ -2,11 +2,7 @@
 import Category from './Category.vue';
 <!-- ApplicationNavigationBar.vue -->
 <template>
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/category">Category</router-link>
-      <router-link to="/about">About</router-link>
-    </nav>
+  
     <nav
     class="navbar navbar-expand-lg navbar-light"
     style="background-color: #f986a3" 
